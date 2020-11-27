691e0e9871dff20e5175355a95eab4327c67868f
ivanryzhankou <75015548+ivanryzhankou@users.noreply.github.com>
2020-11-27 18:37:22 +0300

    Implemented logging

--- lab-file-storage/lab-02/Program.cs
+++ lab-file-storage/lab-02/Program.cs
@@ -13,10 +13,28 @@ namespace lab_02
         static void Main(string[] args)
         {
             Logging();
-
-            Console.WriteLine(ConfigurationManager.AppSettings.Get("repositoryAddress"));
+            ShowStorageContents();
         }
 
+        private static void ShowStorageContents ()
+        {
+            Directory.CreateDirectory(ConfigurationManager.AppSettings.Get("repositoryAddress"));
+            List <string> filesInStorage = new List <string> (Directory.GetFiles(ConfigurationManager.AppSettings.Get("repositoryAddress")));
+            foreach (string file in filesInStorage)
+            {
+                Console.WriteLine(file.Remove(0, (ConfigurationManager.AppSettings.Get("repositoryAddress").Length + 1)));
+            }
+        }
+
+        private static int ShowMenu( List<string>listOfOptions)
+        {
+            int activeOption = 0;
+
+            foreach (string file in listOfOptions)
+            {
+                Console.WriteLine(file);
+            }
+        }
         //private static void Test()
         //{
         //    Directory.CreateDirectory("c:\\Users\\i.ryzhankou\\Desktop\\test");
@@ -31,55 +49,55 @@ namespace lab_02
         //    Console.WriteLine("ok");
         //}
 
-        private static void UpdateSetting(string key, string value)
-        {
-            Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            configuration.AppSettings.Settings[key].Value = value;
-            configuration.Save();
+        //private static void UpdateSetting(string key, string value)
+        //{
+        //    Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        //    configuration.AppSettings.Settings[key].Value = value;
+        //    configuration.Save();
 
-            ConfigurationManager.RefreshSection("appSettings");
-        }
+        //    ConfigurationManager.RefreshSection("appSettings");
+        //}
         private static void Logging()
         {
-            bool validUser; // Valid variable name?
+            bool isUserValid; // Valid variable name?
 
             do
             {
-                string login = GetStringFromUser("Enter your login");
+                UserNotice("Enter your login");
+                string login = GetStringFromUser();
 
-                string password = GetStringFromUser("Enter your password");
+                UserNotice("Enter your password");
+                string password = GetStringFromUser();
 
-                validUser = UserVerification(login, password);
+                isUserValid = UserVerification(login, password);
 
-                if (validUser == false)
+                if (isUserValid == false)
                 {
                     Console.Clear();
                     UserNotice("Try again. Wrong login or password.");
                 }
             }
+            while (isUserValid == false);
 
-            while (validUser == false);
-
-            if (ConfigurationManager.AppSettings.Get("repositoryAddress") == "null")
-            {
-                RepositoryCreation();
-            }
+            //if (ConfigurationManager.AppSettings.Get("repositoryAddress") == "null")
+            //{
+            //    RepositoryCreation();
+            //}
         }
 
-        private static void RepositoryCreation()
-        {
-            UpdateSetting("repositoryAddress", GetStringFromUser("enter repository address"));
-        }
+        //private static void RepositoryCreation()
+        //{
+        //    UpdateSetting("repositoryAddress", GetStringFromUser("enter repository address"));
+        //}
 
         private static bool UserVerification(string login, string password)
         {
            return login == ConfigurationManager.AppSettings.Get("login") && password == ConfigurationManager.AppSettings.Get("password");
         }
 
-        private static string GetStringFromUser(string message)
+        private static string GetStringFromUser()
         {
             {
-                Console.WriteLine(message);
                 return Console.ReadLine();
             }
         }
