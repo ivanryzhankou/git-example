ac40770f1d0f3fd2916883abb478623f0128681b
ivanryzhankou <75015548+ivanryzhankou@users.noreply.github.com>
2020-11-27 17:28:07 +0300

    Task 2 was started
    Created a repository for the second task and proceeded to execute

---
+++ lab-file-storage/lab-02/Interface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_02
+{
+    class Interface
+    {
+        private static void ShowInterface ()
+        {
+            Console.WriteLine ("Show your storage");
+            Console.Write("Show user info");
+        }
+    }
+
+}
---
+++ lab-file-storage/lab-02/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace lab_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logging();
+
+            Console.WriteLine(ConfigurationManager.AppSettings.Get("repositoryAddress"));
+        }
+
+        //private static void Test()
+        //{
+        //    Directory.CreateDirectory("c:\\Users\\i.ryzhankou\\Desktop\\test");
+        //    Directory.Delete("c:\\Users\\i.ryzhankou\\Desktop\\test");
+        //    string a = "c:\\Users\\i.ryzhankou\\Desktop\\test";
+        //    var txtFiles = Directory.EnumerateFiles(a);
+
+        //    foreach (string str in txtFiles)
+        //    {
+        //        Console.WriteLine(str.Remove(0, a.Length + 1));
+        //    }
+        //    Console.WriteLine("ok");
+        //}
+
+        private static void UpdateSetting(string key, string value)
+        {
+            Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            configuration.AppSettings.Settings[key].Value = value;
+            configuration.Save();
+
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+        private static void Logging()
+        {
+            bool validUser; // Valid variable name?
+
+            do
+            {
+                string login = GetStringFromUser("Enter your login");
+
+                string password = GetStringFromUser("Enter your password");
+
+                validUser = UserVerification(login, password);
+
+                if (validUser == false)
+                {
+                    Console.Clear();
+                    UserNotice("Try again. Wrong login or password.");
+                }
+            }
+
+            while (validUser == false);
+
+            if (ConfigurationManager.AppSettings.Get("repositoryAddress") == "null")
+            {
+                RepositoryCreation();
+            }
+        }
+
+        private static void RepositoryCreation()
+        {
+            UpdateSetting("repositoryAddress", GetStringFromUser("enter repository address"));
+        }
+
+        private static bool UserVerification(string login, string password)
+        {
+           return login == ConfigurationManager.AppSettings.Get("login") && password == ConfigurationManager.AppSettings.Get("password");
+        }
+
+        private static string GetStringFromUser(string message)
+        {
+            {
+                Console.WriteLine(message);
+                return Console.ReadLine();
+            }
+        }
+
+        private static void UserNotice(string message)
+        {
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
